9fdc16e0fd75c667cd8e612d7675bcd2e9dee22c
ackwolver335 <abhaych335@gmail.com>
2024-05-29 22:30:37 +0530

    Classes and Object code Concept added

--- OOps.java
+++ OOps.java
@@ -2,6 +2,7 @@ public class OOps{
     // defining some variables here
     static String name;
     static int age;
+    int id;
 
     // defining main class static method
     static void set(String n,int a){
@@ -20,5 +21,38 @@ public class OOps{
         // Calling the methods directly by the class name
         OOps.set("Ack Wolver", 21);
         OOps.get();
+        OOps obj1 = new OOps();
+        obj1.id = 23;
+        System.out.println("Value of Default variable id : " + obj1.id);            // Created an object
+
+        // Creating an Object as another example
+        cat obj2 = new cat("Mandy","American",12,"White");
+        System.out.println(obj2.getall());
+    }
+}
+
+class cat{
+    // Default Data Members or Instances
+    String name;
+    String breed;
+    int age;
+    String colour;
+    
+    // defining the constructor
+    public cat(String name,String breed,int age,String colour){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.colour = colour;
+    }
+
+    // Defining different methods of it
+    public String getname() { return name; }
+    public String getbreed() { return breed; }
+    public int getage() { return age; }
+    public String getclr() { return colour; }
+
+    public String getall(){
+        return ("Hi my name is : " + name + "\nMy Breed is : " +  breed + "\nMy Age is : " + age + "\nMy Colour is : " + colour);
     }
 }
